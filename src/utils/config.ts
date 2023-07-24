21f7e00192a20e136abfd91f8931348a22abdd00
Fairy-Phy <phy.public@gmail.com>
2023-07-25 02:02:06 +0900

    数値が0のときに自動的にデフォルト値になってしまう問題を修正

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,8 +5,12 @@ const envFile = process.env.NODE_ENV === 'development' ? '.env.development' : '.
 require('dotenv').config({ path: `./${envFile}` })
 
 const today = new Date()
-const recordTimeHour = Number(process.env?.RECORD_HOUR) || 3
-const recordTimeSEC = Number(process.env?.RECORD_MINUTE) || 34
+const recordTimeHourStr = process.env?.RECORD_HOUR
+const recordTimeHourRaw = Number(typeof recordTimeHourStr === "string" && recordTimeHourStr.trim().length !== 0 ? recordTimeHourStr : NaN)
+const recordTimeHour = isFinite(recordTimeHourRaw) ? recordTimeHourRaw : 3
+const recordTimeSECStr = process.env?.RECORD_MINUTE
+const recordTimeSECRaw = Number(typeof recordTimeSECStr === "string" && recordTimeSECStr.trim().length !== 0 ? recordTimeSECStr : NaN)
+const recordTimeSEC = isFinite(recordTimeSECRaw) ? recordTimeSECRaw : 34
 export const recordTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), recordTimeHour, recordTimeSEC, 0, 0)
 
 export const postTitle = process.env?.POST_TITLE || `Today's 334 Top 10`
