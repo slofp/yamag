458b49d07726aed99a5a3b622a2ddb2b471ff389
Fairy-Phy <phy.public@gmail.com>
2023-07-28 00:44:22 +0900

    リマインドのエラー再送

--- src/post_remind.ts
+++ src/post_remind.ts
@@ -4,6 +4,8 @@ import { recordTime, remindPostText } from '@/utils/config'
 let now = new Date()
 const getTimeDiff = ():number => recordTime.getTime() - new Date().getTime();
 
+const MAX_RETRY_COUNT = 3
+
 async function main() {
   console.log("Remind Post Script Launched")
   let currentDiff = getTimeDiff()
@@ -12,7 +14,17 @@ async function main() {
       await new Promise(resolve => setTimeout(resolve, 50))
     }
     console.log(new Date())
-    YAMAG.Misskey.postNote(`${remindPostText}(${now.toLocaleDateString('ja-JP')})`)
+    for (let i = 0; i < MAX_RETRY_COUNT; i++) {
+      try {
+        await YAMAG.Misskey.postNote(`${remindPostText}(${now.toLocaleDateString('ja-JP')})`)
+        break
+      }
+      catch (err) {
+        console.log("Post error. post after 3s")
+        console.debug(err)
+        await new Promise(r => setTimeout(r, 3000))
+      }
+    }
   }
 }
 
